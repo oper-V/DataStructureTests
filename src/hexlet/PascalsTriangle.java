f7abd468f0743f086d25f2a9d52387b074099faf
Igor Prokhorenko <igor.prohorenko@gmail.com>
2021-03-20 09:43:15 +0200

    Скопированы заготовки для задания "Треугольник Паскаля"

---
+++ src/hexlet/PascalsTriangle.java
@@ -0,0 +1,13 @@
+package hexlet;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PascalsTriangle {
+
+    public static ArrayList<Integer> triangle(int rowNumber) {
+        //BEGIN (write your solution here)
+
+        //END
+    }
+}
