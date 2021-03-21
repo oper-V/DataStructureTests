e4c05df62c7c5e08646b33bfabba13c0033d24e8
Igor Prokhorenko <igor.prohorenko@gmail.com>
2021-03-21 19:13:30 +0200

    Код задания "Треугольник Паскаля" написан, и проходит тесты.

--- src/hexlet/PascalsTriangle.java
+++ src/hexlet/PascalsTriangle.java
@@ -15,16 +15,20 @@ public class PascalsTriangle {
 
     public static ArrayList<Integer> triangle(int rowNumber) {
         //BEGIN (write your solution here)
-
+    /*
+    Чтобы найти следующую строку в треугольнике, нужно взять предыдущую,
+    и прибавить к ней точно такую же, но сдвинутую на одну колонку вправо.
+    */
         ArrayList<Integer> prevLine = new ArrayList<>();
         ArrayList<Integer>  resultLine = new ArrayList<>();
 
         for( int i = 0; i <= rowNumber; i++) {
             for (int j = 0; j <= i; j++) {
-                if (j == 0) {
+                if (j == 0) { //если первое число в строке, то это 1
                     resultLine.clear();
                     resultLine.add(1);
                 } else {
+                    // Иначе
                     resultLine.add(j, (prevLine.get(j) + prevLine.get(j - 1)));
                 }
             }
