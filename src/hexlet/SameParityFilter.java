d6c96373b5b631e15e641dbccbd9da4413b05434
Igor Prokhorenko <igor.prohorenko@gmail.com>
2021-03-22 22:30:36 +0200

    Код испытания "Одинаковая чётность" проходит тесты.

--- src/hexlet/SameParityFilter.java
+++ src/hexlet/SameParityFilter.java
@@ -4,8 +4,24 @@ import java.util.ArrayList;
 
 public class SameParityFilter {
     //BEGIN (write your solution here)
+
     public static ArrayList<Integer> same_parity_filter(ArrayList<Integer> inputList) {
-        return null;
+        ArrayList<Integer> result = new ArrayList<>();
+        if (inputList.size() == 0) return result; //Проверка на пустой массив
+        if (isEven(inputList.get(0))) {
+            for (Integer i : inputList) {
+                if (isEven(i)) result.add(i);
+            }
+        } else {
+            for (Integer i : inputList) {
+                if (!isEven(i)) result.add(i);
+            }
+        }
+        return result;
+    }
+
+    private static boolean isEven(Integer number) {
+        return number.intValue() % 2 == 0;
     }
 
     //END
