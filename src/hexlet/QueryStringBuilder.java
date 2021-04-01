2831ce68c282740292069de42feaf573edc83dc2
Igor Prokhorenko <igor.prohorenko@gmail.com>
2021-04-02 00:50:55 +0300

    Созданы заготовки классов для испытания "Сборщик строки запроса" и тесты для них.

---
+++ src/hexlet/QueryStringBuilder.java
@@ -0,0 +1,14 @@
+package hexlet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class QueryStringBuilder {
+
+    //BEGIN (write your solution here)
+
+    //END
+}
---
+++ tests/hexlet/QueryStringBuilderTest.java
@@ -0,0 +1,23 @@
+import static hexlet.QueryStringBuilder.buildQueryString;
+
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class QueryStringBuilderTest {
+
+    @Test
+    public void testBuildQueryString() {
+
+        Assert.assertEquals(buildQueryString(Map.of("page", "1")), "page=1");
+        Assert.assertEquals(buildQueryString(Map.of("per", "10", "page", "1")), "page=1&per=10");
+        Assert.assertEquals(buildQueryString(Map.of(
+                "a", "10",
+                "s", "Wow",
+                "d", "3.2",
+                "z", "89"
+        )), "a=10&d=3.2&s=Wow&z=89");
+    }
+}
+
