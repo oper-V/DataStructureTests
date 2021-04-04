243b1869e5784a53423dfbc30f8f0adc7f3cf63a
Igor Prokhorenko <igor.prohorenko@gmail.com>
2021-04-04 09:34:31 +0300

    Испытание "Слияние словарей" проходит тесты

--- src/hexlet/DictionariesMerge.java
+++ src/hexlet/DictionariesMerge.java
@@ -8,6 +8,51 @@ import java.util.Map;
 public class DictionariesMerge {
 
     //BEGIN (write your solution here)
+    public static Map<String, Set<Integer>> merged(Map ... dictionaries) {
+        Set dictKeys = new HashSet();
+        HashMap<String, Set<Integer>> result = new HashMap<>();
+
+        for (Map dict : dictionaries) {
+            for ( Object key : dict.keySet()) {
+                if(!result.containsKey(key)) {
+                    //result.put((String )key, (Set<Integer>) dict.get(key));
+                    result.put((String )key, new HashSet<Integer>());
+                }
+            }
+        }
+
+       for(String id : result.keySet()) {
+           Set values = new HashSet();
+           for (Map dict : dictionaries) {
+               for ( Object key : dict.keySet()) {
+                   if(key.equals(id)) { // в result есть
+                       values.add(dict.get(id));
+                   }
+               }
+           }
+           result.put(id, values);
+        }
+
+        return  result;
+    }
+    // Решение учителя. Важный момент, уточнение типа МАПЫ в сигнатуре метода
+    // Не знакомое мне применение лямбда-выражений
+    public static Map<String, Set<Integer>> merged1(Map<String, Integer>... dicts) {
+
+        Map<String, Set<Integer>> aggregate = new HashMap<String, Set<Integer>>(Map.of());
+        for (Map<String, Integer> item : dicts) {
+            for (Map.Entry<String, Integer> entry : item.entrySet()) {
+                aggregate.merge(entry.getKey(), Set.of(entry.getValue()),
+                        (oldValue, newValue) -> {
+                            Set<Integer> result = new HashSet<Integer>();
+                            result.addAll(oldValue);
+                            result.addAll(newValue);
+                            return result;
+                        });
+            }
+        }
+        return aggregate;
+    }
 
     //END
 }
--- tests/hexlet/DictionariesMergeTest.java
+++ tests/hexlet/DictionariesMergeTest.java
@@ -17,20 +17,25 @@ public class DictionariesMergeTest {
                 Map.of("A", 1),
                 Map.of("B", 2)
         ), Map.of("A", Set.of(1), "B", Set.of(2)));
+
         Assert.assertEquals(merged(
                 Map.of("A", 1),
                 Map.of("A", 3),
                 Map.of("A", 4),
                 Map.of("B", 2)
         ), Map.of("A", Set.of(1,3,4), "B", Set.of(2)));
+
         Assert.assertEquals(merged(
                 Map.of("A", 1),
                 Map.of("A", 2)
         ), Map.of("A",  Set.of(1, 2)));
+
         Assert.assertEquals(merged(
                 Map.of(),
                 Map.of()
         ), Map.of());
+
+
     }
 }
 
